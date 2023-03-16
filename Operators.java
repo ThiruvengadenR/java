015e9c3d6f671c3917e5c6b04f78f8249bba41c1
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-03-16 18:21:20 +0530

    Operator code numbers

---
+++ Operators.java
@@ -0,0 +1,17 @@
+class Operators
+{
+public static void main(String [] args)
+{
+char alphet = 't';
+boolean Tf = true;
+byte hari = -45;
+short harini = 555;
+long harinir = 654212;
+double hariniThiru = 45.478855455565;
+double hariniSelvi = 754.1;
+int harinivenkat = 6874676;
+
+System.out.println(hari+harini+harinir+hariniThiru+hariniSelvi+harinivenkat);
+}
+}
+
