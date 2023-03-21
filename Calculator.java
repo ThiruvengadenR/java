0204da93a84ea57efddc0dd91709e6e214d48269
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-03-21 15:01:10 +0530

    lets enter into object creation and method calling

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+class Calculator
+{
+public static void main(String[] args)
+{
+Calculator calc = new Calculator();
+calc.add();
+calc.sub();
+calc.multi();
+calc.divi();
+}
+void add()
+{
+System.out.println(10+20);
+}
+void sub()
+{
+System.out.println(10-20);
+}
+void multi()
+{
+System.out.println(10*20);
+}
+void divi()
+{
+System.out.println(10/20);
+}
+}
