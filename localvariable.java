0eb7bbf886981f2ddb25b71e039aa0b209b87a4d
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-08 10:15:09 +0530

    local variables

---
+++ localvariable.java
@@ -0,0 +1,15 @@
+class Home
+{
+public static void main(String [] args)
+{
+Home fan = new Home();
+fan.run();
+}
+
+void run()
+{
+int Switch_no = 5;
+System.out.println("please turn on the switch " +  Switch_no);
+}
+}
+
