ef232841637eff233deec1545abb0e6d0fa6f00b
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-12 15:08:04 +0530

    zero constructor

---
+++ zero_arg_constructor.java
@@ -0,0 +1,19 @@
+class People 
+{
+public static void main (String [] args )
+{
+People religion1 = new People("Hindu");
+People religion2 = new People("muslims");
+People religion3 = new People("christian");
+People citizen = new People();
+}
+People (String india)
+{
+System.out.println("unity in diversity is our power");
+}
+
+People ()
+{
+System.out.println("I am also a indain love india ");
+}
+}
