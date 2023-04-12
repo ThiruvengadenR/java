ab32b68bf1ee48940e2c63ab46d2fa31d0773ca9
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-12 15:17:18 +0530

    local and global

---
+++ same_name.java
@@ -0,0 +1,13 @@
+class Industrial_visit
+{
+int amount = 5000;
+public static void main (String [] args)
+{
+Industrial_visit mechnical = new Industrial_visit();
+mechnical.fun(500);
+}
+void fun(int amount)
+{
+System.out.println("fun amount is  " + amount);
+}
+}
