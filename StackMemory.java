5965b310b4382a9a2a504640ac1ba4c21bc8734f
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-12 15:02:01 +0530

    stackmemory

---
+++ StackMemory.java
@@ -0,0 +1,28 @@
+class Boy
+{
+public static void main (String [] args)
+{
+Boy Thiru = new Boy();
+Thiru.GoToShop();
+}
+void GoToShop()
+{
+System.out.println("I get my bike");
+Purchase();
+}
+void Purchase()
+{
+System.out.println("I want two blue pen");
+Give_cash();
+}
+void Give_cash()
+{
+System.out.println("TAKE THE 100 RUPEES");
+Get_balance();
+}
+void Get_balance()
+{
+System.out.println("Thiru take the return amount 100");
+}
+}
+
