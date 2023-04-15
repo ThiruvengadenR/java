5080a7c8137f8f5c440515316dfdd3fe48dd053b
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-15 08:45:55 +0530

    package source

---
+++ bankOfBaroda.java
@@ -0,0 +1,19 @@
+package tamilnadu.thiruvarur;
+public class bankOfBaroda  
+{
+static int  minimum_balance = 500;
+public static void main (String [] args)
+{
+bankOfBaroda manager = new bankOfBaroda();
+manager.loan();
+manager.gold_rate();
+}
+void loan()
+{
+System.out.println("first step i check your credit score");
+}
+public void gold_rate()
+{
+System.out.println(" per gram rupes 3500");
+}
+}
