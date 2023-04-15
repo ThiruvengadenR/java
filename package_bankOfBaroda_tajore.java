ef6bd90b6167c99ad3cfc8a250350dff1a830f38
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-15 08:48:24 +0530

    package extends of bank of baraoda

---
+++ package_bankOfBaroda_tajore.java
@@ -0,0 +1,11 @@
+package tamilnadu.tajore;
+import tamilnadu.thiruvarur.bankOfBaroda ;
+class bankOfBaroda_tajore extends bankOfBaroda 
+{
+public static void main (String [] args )
+{
+bankOfBaroda_tajore cashier = new bankOfBaroda_tajore();
+cashier.gold_rate();
+}
+}
+
