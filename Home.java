275a388399fddb443987d0fc6872c4f9b0e1705f
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-18 22:02:50 +0530

    final keyword

---
+++ Home.java
@@ -0,0 +1,17 @@
+public class Home
+{
+final String home_name = "selvi's home";
+int monthly_salary = 10000;
+public static void main (String [] args)
+{
+Home sweat_home = new Home();
+sweat_home.change_name();
+}
+void change_name()
+{
+home_name = "thiru's home";
+}
+}
+
+
+
