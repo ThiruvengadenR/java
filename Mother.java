516197bf21795dae05c3f157e2113b88d2a3f194
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-04-18 22:08:11 +0530

    parent class

---
+++ Mother.java
@@ -0,0 +1,15 @@
+class Mother
+{
+static int age = 50;
+static int salary = 8000; 
+public static void main (String [] args)
+{
+Mother cook =  new Mother();
+cook.dosa();
+}
+void dosa ()
+{
+System.out.println("take the tasty dosa");
+}
+}
+
